4c9f81695d90d87fc36ee8c9c8d95e5c44ff34ef
Nexis Sis <nexis.sis@gmail.com>
2018-08-13 18:55:54 +0300

    carousel

--- src/blocks/_common/main.js
+++ src/blocks/_common/main.js
@@ -1,1 +1,3 @@
+
 //= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/jcarousel/dist/jquery.jcarousel.js
---
+++ src/blocks/index/testimonials/testimonials.js
@@ -0,0 +1,36 @@
+
+//carousel
+(function($) {
+    $(function() {
+        $('.testimonials-carousel').jcarousel(
+            {
+                wrap: 'both',
+                animation: 'slow'
+            }
+        );
+
+        $('.testimonials-control-back')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.testimonials-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+    });
+})(jQuery);
+
